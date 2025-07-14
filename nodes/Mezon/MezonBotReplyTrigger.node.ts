1b248b9f21c17bc88f7314270e72dadc4a9e7219
Duong Nguyen Dai <hello@mrdnd.dev>
2025-07-14 23:17:42 +0700

    add multi channels & multi sender limit handler

--- nodes/Mezon/MezonBotReplyTrigger.node.ts
+++ nodes/Mezon/MezonBotReplyTrigger.node.ts
@@ -72,6 +72,7 @@ export class MezonBotReplyTrigger implements INodeType {
 		let credential = await this.getCredentials<MezonCredentials>('mezonApi');
 		var client = new MezonClient(credential.apiKey);
 		await client.login();
+		console.log(`n8n-nodes-mezon: Bot ${credential.appId} started`);
 		var onMentionedOnly = this.getNodeParameter('onMentionedOnly') as boolean;
 		var senderId = this.getNodeParameter('senderId') as string;
 		var clanId = this.getNodeParameter('clanId') as string;
@@ -83,24 +84,43 @@ export class MezonBotReplyTrigger implements INodeType {
 				// Ignore my message
 				return;
 			}
-			if (senderId != '' && event.sender_id != senderId) {
+			if (onMentionedOnly && !event?.mentions?.find((mention) => mention.user_id === credential.appId)) {
 				return;
 			}
 			if (clanId != '' && event.clan_id != clanId) {
 				return;
 			}
-			if (channelId != '' && event.channel_id != channelId) {
-				return;
-			}
-			if (onMentionedOnly && !event?.mentions?.find((mention) => mention.user_id === credential.appId)) {
-				return;
-			}
-			const channelFetch = await client.channels.fetch(event.channel_id)
+			const channelFetch = await client.channels.fetch(event.channel_id);
 			if (!event.message_id) {
 				console.warn("event.message_id is undefined, skipping message fetch.");
 				return;
 			}
 			const messageFetch = await channelFetch.messages.fetch(event.message_id);
+			if (senderId != '') {
+				if (senderId.indexOf(',') >= 0) {
+					const senderIds = senderId.split(',');
+					if (!senderIds.includes(event.sender_id)) {
+						messageFetch.reply({ t: "You are not allowed to trigger this bot." });
+						return;
+					}
+				} else if (event.sender_id != senderId) {
+					messageFetch.reply({ t: "You are not allowed to trigger this bot." });
+					return;
+				}
+			}
+			if (channelId != '') {
+				if (channelId.indexOf(',') >= 0) {
+					const channelIds = channelId.split(',');
+					if (!channelIds.includes(event.channel_id)) {
+						messageFetch.reply({ t: "You are not allowed to trigger this bot in this channel." });
+						return;
+					}
+				} else if (event.channel_id != channelId) {
+					messageFetch.reply({ t: "You are not allowed to trigger this bot in this channel." });
+					return;
+				}
+			}
+
 			// call to URL and get response
 			const response = await fetch(botLogicUrl, {
 				method: "POST",
@@ -116,7 +136,7 @@ export class MezonBotReplyTrigger implements INodeType {
 		// The "closeFunction" function gets called by n8n whenever
 		// the workflow gets deactivated and can so clean up.
 		const closeFunction = async () => {
-			console.log("n8n-nodes-mezon: closeFunction triggered");
+			console.log(`n8n-nodes-mezon: ${credential.appId} stopped`);
 			client.closeSocket();
 		};
 
